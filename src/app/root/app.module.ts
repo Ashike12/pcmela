786dd34da3e9d3a85892c891fd17d5dbb87f85d9
Ashikur Rahman Nabir <ashike.cse.bd@gmail.com>
2019-10-06 11:30:04 +0600

    naming convention fixed

--- src/app/root/app.module.ts
+++ src/app/root/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { CommonMaterialModule } from "../common-module/CommonMaterialModule";
+import { CommonMaterialModule } from "../common-module/common-material.module";
 import { AppRoutingModule } from './app-routing.module';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import { AppRootDefaultComponent } from './root-default/root-default.component';
