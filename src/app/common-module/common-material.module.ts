9926ccb88989338c2d3c057da1b2aa1d369410eb
Ashikur Rahman Nabir <ashike.cse.bd@gmail.com>
2019-11-27 00:44:19 +0600

    ui change

--- src/app/common-module/common-material.module.ts
+++ src/app/common-module/common-material.module.ts
@@ -40,6 +40,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   exports: [
     A11yModule,
@@ -83,7 +84,8 @@ import { MatTreeModule } from '@angular/material/tree';
     MatTooltipModule,
     MatTreeModule,
     PortalModule,
-    ScrollingModule,
+    FlexLayoutModule,
+    ScrollingModule
   ]
 })
 export class CommonMaterialModule {
